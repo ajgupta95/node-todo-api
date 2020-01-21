eb3117eb939b6826f63bb51fc4e3aa1ae1970b1d
Kumar Ujjawal <kumarujjawal786@gmail.com>
2020-01-21 14:08:42 +0530

    inital commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,47 @@
+ const MongoClient= require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
+    if(err){
+        return console.log("connection did not made");
+    }
+    console.log("connected to MongoDb server");
+    var db = client.db('TodoApp');
+     /* db.collection('Users').insertOne({
+        name: 'Ajay',
+        age:24,
+        location:"noida 70 sector"   
+
+
+    },(err,result)=>{
+       if(err){
+           return console.log('unable to insert user',err);
+
+       }
+       console.log((result.ops));
+      
+    });
+    db.collection('Users').insertOne({
+        name: 'Vijay',
+        age:27,
+        location:"noida 70 sector"   
+
+
+    },(err,result)=>{
+       if(err){
+           return console.log('unable to insert user',err);
+
+       }
+       console.log((result.ops));
+      
+    });
+    
+
+    client.close();
+    */
+   db.collection('Users').find({age:25}).count().then((docs)=>{
+       console.log(docs);
+   },(err)=>{
+       console.log("did not find")
+   });
+}); 
+
